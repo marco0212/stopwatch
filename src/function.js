de1967ac2895a091bf711a93f845c8ad3165c483
Jeong <inyeop0212@gmail.com>
2019-12-01 18:08:20 +0900

    Fix convertToTime function

--- src/function.js
+++ src/function.js
@@ -4,6 +4,11 @@ function convertToTime(time) {
     second = Math.floor((time / 100) % limit),
     minute = Math.floor((time / 100 / limit) % limit),
     hour = Math.floor(time / 100 / limit / limit);
+
+  if (second < 10) second = "0" + second.toString();
+  if (minute < 10) minute = "0" + minute.toString();
+  if (milisec < 10) milisec = "0" + milisec.toString();
+
   if (!hour) return `${minute}: ${second}. ${milisec}`;
   return `${hour}: ${minute}: ${second}. ${milisec}`;
 }
