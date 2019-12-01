e8da8aeff0a282831a63e05d5cd2326cd4a2da27
Jeong <inyeop0212@gmail.com>
2019-12-01 15:45:53 +0900

    Add tick function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React, { Component } from "react";
-
-function App() {
-  return "App";
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      milisec: 0
+    };
+  }
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      this.tick();
+    }, 10);
+  }
+  tick() {
+    this.setState({ milisec: this.state.milisec + 1 });
+  }
+  render() {
+    return this.state.milisec;
+  }
 }
-
-export default App;
