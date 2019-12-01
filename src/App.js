737145481e107db015eb0bbf8f0db82bf114351e
Jeong <inyeop0212@gmail.com>
2019-12-01 16:05:37 +0900

    Add toggleStart function

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      milisec: 0
+      milisec: 0,
+      isLive: false
     };
   }
-  componentDidMount() {
-    this.timer = setInterval(() => {
-      this.tick();
-    }, 10);
+  toggleStart() {
+    const isLive = this.state.isLive;
+    if (isLive) {
+      clearInterval(this.timer);
+    } else {
+      this.timer = setInterval(() => {
+        this.tick();
+      }, 10);
+    }
+    this.setState({ isLive: !isLive });
   }
   tick() {
     this.setState({ milisec: this.state.milisec + 1 });
