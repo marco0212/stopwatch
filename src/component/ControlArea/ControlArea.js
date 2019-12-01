5afcf0cd183b8e577f88c47d28d8230223c844c0
Jeong <inyeop0212@gmail.com>
2019-12-01 17:16:06 +0900

    Create utilButton.js

--- src/component/ControlArea/ControlArea.js
+++ src/component/ControlArea/ControlArea.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ToggleStartButton from "./toggleStartButton/toggleStartButton";
+import UtilButton from "./UtilButton/UtilButton";
 
 export default class ControlArea extends React.Component {
   render() {
     return (
       <div className="control-area">
+        <UtilButton />
         <ToggleStartButton toggleStart={this.props.toggleStart} />
       </div>
     );
---
+++ src/component/ControlArea/UtilButton/UtilButton.js
@@ -0,0 +1,6 @@
+import React from "react";
+export default class UtilButton extends React.Component {
+  render() {
+    return <button>UtilButton</button>;
+  }
+}
