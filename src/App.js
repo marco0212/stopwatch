7b009901d493e73550733416309d80f8ba62fe35
Jeong <inyeop0212@gmail.com>
2019-12-01 15:58:45 +0900

    Add convertToTime function

--- src/App.js
+++ src/App.js
@@ -18,3 +18,13 @@ export default class App extends Component {
     return this.state.milisec;
   }
 }
+
+function convertToTime(time) {
+  const limit = 60;
+  let milisec = time % 100,
+    second = Math.floor((time / 100) % limit),
+    minute = Math.floor((time / 100 / limit) % limit),
+    hour = Math.floor(time / 100 / limit / limit);
+  if (!hour) return `${minute}: ${second}. ${milisec}`;
+  return `${hour}: ${minute}: ${second}. ${milisec}`;
+}
